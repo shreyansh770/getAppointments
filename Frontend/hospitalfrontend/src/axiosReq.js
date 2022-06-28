4c1eee5676a3de6ebe14375571238b7b9d9f8555
shreyansh thakur <shreyanshthakur1@gmail.com>
2022-06-28 21:39:52 +0530

    backend deployed

--- Frontend/hospitalfrontend/src/axiosReq.js
+++ Frontend/hospitalfrontend/src/axiosReq.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({baseURL:'http://localhost:8080'})
+const API = axios.create({baseURL:'https://hospital-servers.herokuapp.com/'})
 
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('user')){
